d2f0233637de4fc4fb603ff6fc91b3cef5b6800e
jrsnleons <jrsnleons@users.noreply.github.com>
2020-06-24 14:24:33 +0800

    update status

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("you 0_0", {type: "WATCHING"});
+    bot.user.setActivity("with love <3", {type: "PLAYING"});
 }
